282827ad16c56fc9ec561dcd73db9a0e6fb11408
Dom Binks <dominic.binks@gmail.com>
2022-07-12 22:52:08 +0100

    Basic working use of flask-socketio

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sdofjhaofjdsp1231aof'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html') 
 
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+    socketio.emit('alert event')
+
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
