0d02e10912d1feb394c9e17d08b787759732e39c
Dom Binks <dominic.binks@gmail.com>
2022-07-12 23:52:07 +0100

    Text sent appears in all client browers

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sdofjhaofjdsp1231aof'
+app.config['SECRET_KEY'] = 'OhJUfTfM4igIiYsfnkOQ'
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
-    socketio.emit('alert event')
+@socketio.on('get line')
+def hand_add_line(json):
+    print('Line submitted: ' + str(json))
+    socketio.emit('add line', json)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
